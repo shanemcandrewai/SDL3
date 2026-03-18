f851f578c5339c2f75e4826d1151d76399eb3e54
shane <s_mcandrew@yahoo.com>
2026-03-18 12:35:37 +0100

    hello.c fixed

--- hello.c
+++ hello.c
@@ -1,4 +1,9 @@
 /*
+  SDL3 hello.c
+  adapted from https://github.com/libsdl-org/SDL/blob/main/docs/hello.c
+  cls && clang-tidy hello.c -checks=* --header-filter=ddd --
+  -ISDL3-3.4.2\include
+
   Copyright (C) 1997-2026 Sam Lantinga <slouken@libsdl.org>
 
   This software is provided 'as-is', without any express or implied
@@ -8,52 +13,43 @@
   Permission is granted to anyone to use this software for any purpose,
   including commercial applications, and to alter it and redistribute it
   freely.
+
 */
 #define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
-// #include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
-
-#include <SDL3/SDL_error.h>
-#include <SDL3/SDL_events.h>
-#include <SDL3/SDL_init.h>
-#include <SDL3/SDL_log.h>
-#include <SDL3/SDL_render.h>
-#include <SDL3/SDL_stdinc.h>
-#include <SDL3/SDL_video.h>
+// #include <SDL3/SDL.h>  //uncomment for release
+#include <SDL3/SDL_error.h>  //clang-tidy
+#include <SDL3/SDL_events.h> //clang-tidy
+#include <SDL3/SDL_init.h>   //clang-tidy
+#include <SDL3/SDL_log.h>    //clang-tidy
+#include <SDL3/SDL_main.h>   //keep uncommented for release
+#include <SDL3/SDL_render.h> //clang-tidy
+#include <SDL3/SDL_stdinc.h> //clang-tidy
+#include <SDL3/SDL_video.h>  //clang-tidy
 
 const int static WIDTH = 800;
 const int static HEIGHT = 800;
 const int static MAX = 255;
-
-// static SDL_Window *window = NULL;
-// static SDL_Renderer *renderer = NULL;
-
-struct AppState
-{
-    SDL_Window * window;
-    SDL_Renderer * renderer;
-};
+const float static SCALE = 4.0F;
+const char static *MESSAGE = "Hello World!";
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
-  /* Create the window */
 
-  SDL_Log("%d", argc);
-  SDL_Log("%s", argv[0]);
-  struct AppState * apps = NULL;
-  
+  SDL_Window *window = NULL;
+  SDL_Renderer *renderer = NULL;
+
+  /* Create the window */
   if (!SDL_CreateWindowAndRenderer("Hello World", WIDTH, HEIGHT,
-                                   SDL_WINDOW_FULLSCREEN, &apps->window, &apps->renderer)) {
+                                   SDL_WINDOW_FULLSCREEN, &window, &renderer)) {
     SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
-  appstate = (void **)apps;
+  appstate[0] = renderer;
   return SDL_APP_CONTINUE;
 }
 
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
-  void * kkk = appstate;
   if (event->type == SDL_EVENT_KEY_DOWN || event->type == SDL_EVENT_QUIT) {
     return SDL_APP_SUCCESS; /* end the program, reporting success to the OS. */
   }
@@ -62,30 +58,25 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate) {
-  SDL_Log("%s", "appstate");
-  struct AppState * apps = appstate;
-  // SDL_Window * window = apps->window;
-  SDL_Renderer * renderer = apps->renderer;
-  const char *message = "Hello World!";
+  SDL_Renderer *renderer = appstate;
   int width = 0;
   int height = 0;
-  const float scale = 4.0F;
 
   /* Center the message and scale it up */
   SDL_GetRenderOutputSize(renderer, &width, &height);
-  SDL_SetRenderScale(renderer, scale, scale);
+  SDL_SetRenderScale(renderer, SCALE, SCALE);
   float xpos =
-      (((float)width / scale) -
-       ((float)SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(message))) /
+      (((float)width / SCALE) -
+       ((float)SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(MESSAGE))) /
       2;
   float ypos =
-      (((float)height / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
+      (((float)height / SCALE) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
 
   /* Draw the message */
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, MAX);
   SDL_RenderClear(renderer);
   SDL_SetRenderDrawColor(renderer, MAX, MAX, MAX, MAX);
-  SDL_RenderDebugText(renderer, xpos, ypos, message);
+  SDL_RenderDebugText(renderer, xpos, ypos, MESSAGE);
   SDL_RenderPresent(renderer);
 
   return SDL_APP_CONTINUE;
