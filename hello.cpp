20db77ac761a43d6921ce4f634a71a83a2eefa03
shane <s_mcandrew@yahoo.com>
2026-03-18 19:17:20 +0100

    hello.cpp error and warning free with .clang-tidy from SDL github

--- hello.cpp
+++ hello.cpp
@@ -65,12 +65,13 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
   /* Center the message and scale it up */
   SDL_GetRenderOutputSize(renderer, &width, &height);
   SDL_SetRenderScale(renderer, SCALE, SCALE);
-  float xpos =
-      ((static_cast<float>(width) / SCALE) -
-       ((static_cast<float>(SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) * SDL_strlen(MESSAGE))) /
-      2;
-  float ypos =
-      ((static_cast<float>(height) / SCALE) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
+  float xpos = ((static_cast<float>(width) / SCALE) -
+                (static_cast<float>(SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) *
+                 SDL_strlen(MESSAGE))) /
+               2;
+  float ypos = ((static_cast<float>(height) / SCALE) -
+                SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) /
+               2;
 
   /* Draw the message */
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, MAX);
