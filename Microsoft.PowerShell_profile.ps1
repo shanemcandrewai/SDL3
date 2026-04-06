617ae8a539014e159992c383ff891b1f22bfca10
shane <shanedm@LSCREATIONx>
2026-04-06 12:25:28 +0200

    emscripten wip

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+# called from C:\Users\shane\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+. $env:HOMEDRIVE$env:HOMEPATH/dev/.venv/3.13/Scripts/Activate.ps1
+cd $env:HOMEDRIVE$env:HOMEPATH/dev/sdl3
