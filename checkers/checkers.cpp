f2698c55e7a7c155691484e7ed99b0f1ecb9c480
shane <shanedm@LSCREATIONx>
2026-04-16 12:09:01 +0200

    gold move5

--- checkers/checkers.cpp
+++ checkers/checkers.cpp
@@ -13,16 +13,27 @@
 #include <SDL3/SDL_video.h>   //clang-tidy
 #include <cmath>
 
-const float static SCALE = 0.75;
+const float static SPRITE_SCALE = 0.75;
+const float static BOARD_SCALE = 0.4;
 const int static WIDTH_FULL_HD = 1920;
 const int static HEIGHT_FULL_HD = 1080;
-const int static WIDTH = static_cast<int>(round(WIDTH_FULL_HD * SCALE / 2));
-const int static HEIGHT = static_cast<int>(round(HEIGHT_FULL_HD * SCALE / 2));
-const int static XPOS_START = static_cast<int>(round(-20 * SCALE));
-const int static YPOS_START = static_cast<int>(round(-70 * SCALE));
-const int static XPOS_STEP = static_cast<int>(round(230 * SCALE));
-const int static YPOS_STEP = static_cast<int>(round(140 * SCALE));
-const int static TOKEN_OFFSET = static_cast<int>(round(40 * SCALE));
+// const int static WIDTH_WUXGA = 1920;
+// const int static HEIGHT_WUXGA = 1200;
+const int static WIDTH_PIXEL8A = 1080;
+const int static HEIGHT_PIXEL8A = 2400;
+#ifndef __EMSCRIPTEN__
+const int static WIDTH = static_cast<int>(round(WIDTH_FULL_HD * SPRITE_SCALE));
+const int static HEIGHT =
+    static_cast<int>(round(HEIGHT_FULL_HD * SPRITE_SCALE));
+#else
+const int static WIDTH = static_cast<int>(round(HEIGHT_PIXEL8A * SPRITE_SCALE));
+const int static HEIGHT = static_cast<int>(round(WIDTH_PIXEL8A * SPRITE_SCALE));
+#endif
+const int static XPOS_START = static_cast<int>(round(-20 * SPRITE_SCALE));
+const int static YPOS_START = static_cast<int>(round(-70 * SPRITE_SCALE));
+const int static XPOS_STEP = static_cast<int>(round(230 * SPRITE_SCALE));
+const int static YPOS_STEP = static_cast<int>(round(140 * SPRITE_SCALE));
+const int static TOKEN_OFFSET = static_cast<int>(round(40 * SPRITE_SCALE));
 
 struct State { // NOLINT altera-struct-pack-align
   SDL_Renderer *renderer;
@@ -40,18 +51,19 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) { // NOLINT
   auto *state = new State; // NOLINT cppcoreguidelines-owning-memory
 
 #ifndef __EMSCRIPTEN__
-  if (!SDL_CreateWindowAndRenderer("Checkers", WIDTH_FULL_HD, HEIGHT_FULL_HD,
+  if (!SDL_CreateWindowAndRenderer("Checkers", WIDTH, HEIGHT,
                                    SDL_WINDOW_RESIZABLE, &window,
                                    &state->renderer)) {
     SDL_Log("Couldn't create window and renderer: %s", // NOLINT
             SDL_GetError());
     return SDL_APP_FAILURE;
   }
-  if (!SDL_SetRenderLogicalPresentation(state->renderer, WIDTH, HEIGHT,
-                                        SDL_LOGICAL_PRESENTATION_STRETCH)) {
-    SDL_Log("SDL_SetRenderLogicalPresentation failed: %s", // NOLINT
-            SDL_GetError());
-  }
+  // if (!SDL_SetRenderLogicalPresentation(state->renderer, WIDTH * 2,
+  //                                       HEIGHT * 2,
+  //                                       SDL_LOGICAL_PRESENTATION_STRETCH)) {
+  //   SDL_Log("SDL_SetRenderLogicalPresentation failed: %s", // NOLINT
+  //           SDL_GetError());
+  // }
   if (!SDL_SetRenderVSync(state->renderer, 1)) {
     SDL_Log("Could not enable VSync! SDL error: %s", // NOLINT
             SDL_GetError());
@@ -115,21 +127,27 @@ SDL_AppResult SDL_AppIterate(void *appstate) { // NOLINT
   SDL_RenderClear(state->renderer);
 
   SDL_FRect dst_rect;
-  for (int xpos = XPOS_START; xpos < WIDTH; xpos += XPOS_STEP) { // NOLINT
-    for (int ypos = YPOS_START; ypos < HEIGHT + YPOS_START;      // NOLINT
-         ypos += YPOS_STEP) {                                    // NOLINT
+  for (int xpos = XPOS_START;                                      // NOLINT
+       xpos < static_cast<int>(                                    // NOLINT
+                  round(static_cast<float>(WIDTH) * BOARD_SCALE)); // NOLINT
+       xpos += XPOS_STEP) {                                        // NOLINT
+    for (int ypos = YPOS_START; ypos < HEIGHT * BOARD_SCALE;       // NOLINT
+         ypos += YPOS_STEP) {                                      // NOLINT
       dst_rect.x = static_cast<float>(xpos);
       dst_rect.y = static_cast<float>(ypos);
-      dst_rect.w = static_cast<float>(state->blueortho->w) * SCALE;
-      dst_rect.h = static_cast<float>(state->blueortho->h) * SCALE;
+      dst_rect.w = static_cast<float>(state->blueortho->w) * SPRITE_SCALE;
+      dst_rect.h = static_cast<float>(state->blueortho->h) * SPRITE_SCALE;
       SDL_RenderTexture(state->renderer, state->tblueortho, nullptr, &dst_rect);
     }
   }
 
-  dst_rect.w = static_cast<float>(state->blueortho->w) * SCALE / 2;
-  dst_rect.h = static_cast<float>(state->blueortho->h) * SCALE / 2;
+  dst_rect.w = static_cast<float>(state->blueortho->w) * SPRITE_SCALE / 2;
+  dst_rect.h = static_cast<float>(state->blueortho->h) * SPRITE_SCALE / 2;
 
-  if ((*state).xpos < WIDTH && (*state).ypos < HEIGHT + YPOS_START) {
+  if ((*state).xpos <
+          static_cast<int>(round(static_cast<float>(WIDTH) * BOARD_SCALE)) &&
+      (*state).ypos <
+          static_cast<int>(round(static_cast<float>(HEIGHT) * BOARD_SCALE))) {
     (*state).xpos += 1;
     (*state).ypos += 1;
   }
